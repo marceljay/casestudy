d8f35c05bc9dd48571e9ea0d86177a94639c0c6d
Marcel <jackisch@protonmail.com>
2019-08-01 19:32:11 +0200

    added code to tidy data frames

--- tidy_data.R
+++ tidy_data.R
@@ -26,19 +26,40 @@ if( !require(lubridate)){
 
 
 # Loading libraries
+
 library(readr)
 library(dplyr)
 library(data.table)
 library(lubridate)
 
-#Import Data
+#Import Einzelteil.csv matching the required format 
 path <- "Data/Einzelteil/Einzelteil_T04.csv"
 
 df <- read_csv2(path)
 df2 <- df
 
-df2$Produktionsdatum_Origin_01011970 <- replace(df$Produktionsdatum_Origin_01011970, is.numeric(df$Produktionsdatum_Origin_01011970), toString(dmy("01-01-1970")+days(df$Produktionsdatum_Origin_01011970)) ) 
+#Store all counted days in vector
+daycount <-df$Produktionsdatum_Origin_01011970
 
+#Check if origin has a single unique value
+if (length(unique(df$origin))==1) {
+  # Reformat date from data frame to fit as.Date
+  betterDates <- as.Date(daycount, origin = "1970-01-01")
+} else {
+  print("Aborting, multiple values found!")
+}
 
+# Add date column with correctly formatted dates
+df2$date <- betterDates
 
+### Tidy: Deleting Columns
 
+#Check if X1 == X1_1
+if (sum(!df$X1 == df$X1_1) == 0) {
+  # Delete X1_1
+  df2$X1_1 <- NULL
+}
+
+# Drop previously date-related columns
+df2$Produktionsdatum_Origin_01011970 <- NULL
+df2$origin <- NULL
