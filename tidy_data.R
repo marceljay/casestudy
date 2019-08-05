665de7ea53ebea826ee5d219f01d309501167954
Marcel <jackisch@protonmail.com>
2019-08-05 21:33:49 +0200

    added functions for cleaning all data sets

--- tidy_data.R
+++ tidy_data.R
@@ -4,8 +4,8 @@
 
 
 # Installation of dependencies
-if( !require(readr)){
-  install.packages("readr")
+if( !require(tidyverse)){
+  install.packages("tidyverse")
 }
 
 if( !require(shiny)){
@@ -30,13 +30,68 @@ if( !require(lubridate)){
 library(readr)
 library(dplyr)
 library(data.table)
-library(lubridate)
+library(lubridate) # For date functions
+library(stringr) # For analyzing strings
 
-#Import Einzelteil.csv matching the required format 
+
+# # CSV with pattern A (clean)
+list_A <- c("T04", "T06", "T08", "T10", "T13", "T14", "T18", "T21", "T25", "T26", "T33", "T37", "T40")
+
+# #CSV with pattern B (dirty)
+list_B <- c("T15", "T23", "T32", "T38")
+
+# #CSV with pattern C (dirty with extra cols)
+list_C <- c("T12", "T17", "T30")
+
+
+#### Importing the data frames
+
+# Get a char vector with all the paths
+pathVector <- list.files("Data/Einzelteil")
+paste("Data/Einzelteil/", pathVector)
+
 path <- "Data/Einzelteil/Einzelteil_T04.csv"
 
+determineTidyFunction <- function(pathVector){
+  for (i in 1:length(pathVector)) {
+    
+  }
+  
+  
+  return(object)
+}
+
+
+tidyCSV_a <- function(path){
+  # return data.frame
+}
+
+
+tidyCSV_b <- function(path){
+  # return data.frame
+}
+
+
+tidyCSV_c <- function(pathVector){
+  # return data.frame
+}
+
+tidyTXT_a <- function(pathVector){
+  # return data.frame
+}
+
+tidyTXT_xyz <- function(pathVector){
+  # return data.frame
+}
+
+
+
+#Read CSV 
 df <- read_csv2(path)
 df2 <- df
+
+#Read TXT ( | | delimited)
+dftxt <- read_table(path2)
 
 #Store all counted days in vector
 daycount <-df$Produktionsdatum_Origin_01011970
