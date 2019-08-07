09f721b7aca9593b228e8d42a783dd00f92cd1a8
Marcel <jackisch@protonmail.com>
2019-08-07 10:20:42 +0200

    increased functionality

--- tidy_data_Einzelteile.R
+++ tidy_data_Einzelteile.R
@@ -1,12 +1,12 @@
 # title: "Case Study Documentatation"
 # author: "IDA Group 7"
-# date: "July 2019"
+# date: "August 2019"
 
 
 # Installation of dependencies
-if( !require(tidyverse)){
-  install.packages("tidyverse")
-}
+# if( !require(tidyverse)){
+#   install.packages("tidyverse")
+# }
 
 if( !require(shiny)){
   install.packages("shiny")
@@ -47,72 +47,87 @@ list_C <- c("T12", "T17", "T30")
 #### Importing the data frames
 
 # Get a char vector with all the paths
-pathVector <- list.files("Data/Einzelteil")
-pathVector <- paste("Data/Einzelteil/", pathVector)
+partFileNames <- list.files("Data/Einzelteil")
+pathVector <- paste("Data/Einzelteil/", partFileNames, sep="")
+
+# #Reduction for testing purposes
+# pathVector <- pathVector[1:5] 
 
 
 determineTidyFunction <- function(pathVector) {
-  for (i in 1:length(pathVector)) {
+  
+  # # Reduction for testing purposes
+  # for (i in 1:5) {
+    
+    for (i in length(pathVector)) {
     if (length(which(str_detect(pathVector[i], list_A))) == 1){
-      print(paste(i, pathVector[i])) # console logging
-      tidyCSV_a(pathVector[i])
+      print(paste("logging:", i, pathVector[i])) # console logging
+      tidyCSV_a(pathVector[i], list_A[i])
     } else if (length(which(str_detect(pathVector[i], list_B))) == 1){
-      print(paste(i, pathVector[i])) # console logging
-      tidyCSV_b(pathVector[i])
+      print(paste("logging:", i, pathVector[i])) # console logging
+      tidyCSV_b(pathVector[i], list_B[i])
     } else if (length(which(str_detect(pathVector[i], list_C))) == 1){
-      print(paste(i, pathVector[i])) # console logging
-      tidyCSV_c(pathVector[i])
+      print(paste("logging:", i, pathVector[i])) # console logging
+      tidyCSV_c(pathVector[i], list_C[i])
     }  else {
+      print(paste("logging:", i, pathVector[i])) # console logging
       print("txt file or multiple matches found, aborting...");
     }
   }
 }
 
+df <- 1 #define data frame
 
-tidyCSV_a <- function(path){
+tidyCSV_a <- function(path, fileName) {
   # return tidy data.frame for pattern a csv
-  print(path)
-  print("called tidy a fn")
+  print(paste0("tidyCSV_a called with path: ", path))
   
   #Read CSV and store in temporary data frame (df)
-  df <- read_csv2(path)
+  df <<- read_csv2(path)
+
   #Store all counted days in vector
-  daycount <-df$Produktionsdatum_Origin_01011970
-  
+  daycount <<-df$Produktionsdatum_Origin_01011970
+
   #Check if origin has a single unique value and reformat
   if (length(unique(df$origin))==1) {
     # Reformat date from data frame to fit as.Date
-    betterDates <- as.Date(daycount, origin = "1970-01-01")
+    betterDates <<- as.Date(daycount, origin = "1970-01-01")
   } else {
     print("Aborting, multiple values found!")
   }
   
   # Add date column with correctly formatted dates
-  df$date <- betterDates
+  df$date <<- betterDates
   
   
   # Tidy: Deleting Columns
   #Check if X1 == X1_1
-  if (sum(!df$X1 == df$X1_1) == 0) {
+  if (sum(!df$X1 == df$X1_1) == 0) {1
     # Delete X1_1
-    df2$X1_1 <- NULL
+    df$X1_1 <<- NULL
   }
   
   # Drop previously date-related columns
-  df2$Produktionsdatum_Origin_01011970 <- NULL
-  df2$origin <- NULL
+  df$Produktionsdatum_Origin_01011970 <<- NULL
+  df$origin <<- NULL
   
+  # Deleting rows that shall be disregarded because of date range
+  df <<- subset(df, !date<"2015-01-01")
+  df <<- subset(df, !date>"2016-12-31")
+  
+  # print(df)
+  return(df)
 }
 
 tidyCSV_b <- function(path){
   # return tidy data.frame for pattern b csv
-  print("called tidy b fn")
+  print("---- called tidy b fn ----")
 }
 
 
 tidyCSV_c <- function(pathVector){
   # return tidy data.frame for pattern c csv
-  print("called tidy c fn")
+  print("---- called tidy c fn ----")
 }
 
 ### Functions to tidy txt data
@@ -124,4 +139,10 @@ tidyTXT_a <- function(pathVector){
 tidyTXT_xyz <- function(pathVector){
   # return data.frame
 }
+
+
+#################################
+
+# Run the function / Script
+determineTidyFunction(pathVector)
 
